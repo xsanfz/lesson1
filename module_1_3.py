c293436d3632776c9eddea6d2456c5b0fd7bfcd4
xsanfz <sergey.myamin@gmail.com>
2024-11-14 13:03:08 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Динамическая типизация"
+name = 'Sergey'
+print(name)
+age = 44
+print(age)
+new_age = 10
+print(age + new_age)
+is_student = True
+print(is_student)
---
+++ task4.py
@@ -0,0 +1,2 @@
+#"4st program"
+print(int((float('123.456') * 10) % 10))
