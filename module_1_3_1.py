404c46e8697ab18a44d9cc6014faa6b308cbe898
xsanfz <sergey.myamin@gmail.com>
2024-11-18 16:22:10 +0400

    Add files via upload

---
+++ module_1_3_1.py
@@ -0,0 +1,6 @@
+#Практическое задание по теме "Переменные"
+number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours_spent) / (number_of_completed_DZ)
+print('Курс:', course_name,', всего задач:', number_of_completed_DZ, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения', time_for_one_task)
---
+++ module_1_3_2.py
@@ -0,0 +1,7 @@
+#Практическое задание по уроку "Строки и индексация строк"
+example = "Sergey"
+print(example[0])
+print(example[-1])
+print(example[3:6])
+print(example[::-1])
+print(example[1:6:2])
