886fa98e6a0d9b550e203701877294ec7b1dc7b6
xsanfz <sergey.myamin@gmail.com>
2024-12-24 13:19:20 +0400

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,35 @@
+#Домашняя работа по уроку "Специальные методы классов"
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+
+h1 = House('ЖК Эльбрус', 10)
+
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
+
+
+
